ed200fb7deb010f62c6c4575b1c56aa752436d15
Qeatrix <elitelektro74@gmail.com>
2023-09-06 03:01:25 +0500

    Small update

--- dist/spinstyle.81952df5.js
+++ dist/spinstyle.81952df5.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44831" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41565" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
